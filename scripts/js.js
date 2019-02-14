405f26ed6a93b3a2c7ba3fa95aa181265b439e42
railsbug <railsbug@gmail.com>
2019-02-14 10:04:09 +0800

    javascriptbug@126.com

--- scripts/js.js
+++ scripts/js.js
@@ -1,7 +1,18 @@
-function duan(){
-    console.log("asdf");
-}
+var xlsx = require('node-xlsx');
+// var sheets = xlsx.parse('./_data/joker.xls');
+
+
+// function duan(){
+//     sheets.forEach(function(sheet){
+//         console.log(sheet['name']);
+//         // for(var rowId in sheet['data']){
+//         //     console.log(rowId);
+//         //     var row=sheet['data'][rowId];
+//         //     console.log(row);
+//         // }
+//     });
+// }
 
 hexo.extend.generator.register('duan',function(locals) {
-  duan();
+//   duan();
 })
